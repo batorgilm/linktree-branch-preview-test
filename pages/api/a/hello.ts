8da461f9814416105953ed40bc4bc6259cbfa3bc
batorgilm <batorgil.m@nest.edu.mn>
2022-11-25 15:05:42 +0800

    testing

--- pages/api/a/hello.ts
+++ pages/api/a/hello.ts
@@ -1,12 +1,10 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import User from '../../../lib/models/User'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import User from '../../../lib/models/User';
 
 const Hello = async (req: NextApiRequest, res: NextApiResponse) => {
-
-    res.json({
-        status: 200,
-        data: 'Hello',
-    })
-
-}
-export default Hello
+  res.json({
+    status: 200,
+    data: 'Hello world waving at new branch',
+  });
+};
+export default Hello;
